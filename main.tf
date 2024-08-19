55886fe271811414c2219aac8f3d679ca19651da
Steve Heydweiller <steve@highdwellercreative.com>
2024-08-18 21:54:47 -0500

    azs

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "vpc" {
 
   name                 = "education"
   cidr                 = "10.0.0.0/16"
-  azs                  = ["us-west-2a", "us-west-2b"]
+  azs                  = ["us-east-2a", "us-east-2b", "us-east-2c"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_dns_hostnames = true
   enable_dns_support   = true
